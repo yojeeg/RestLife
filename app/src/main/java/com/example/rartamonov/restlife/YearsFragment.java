946665fca5f4a03dbab019e3b8773e23fb6d7c06
yojeeg@gmail.com <Roman26061991>
2017-06-15 11:20:33 +0300

    Добавил зачеркнутость прошедшим годам. Немного рефакторинга.

--- app/src/main/java/com/example/rartamonov/restlife/YearsFragment.java
+++ app/src/main/java/com/example/rartamonov/restlife/YearsFragment.java
@@ -1,6 +1,7 @@
 package com.example.rartamonov.restlife;
 
 import android.content.Context;
+import android.graphics.Paint;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,12 +11,16 @@ import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.TextView;
 
+import java.util.Calendar;
+
 public class YearsFragment extends Fragment{
 
     private Context context;
 
     GridView gvYears;
     ArrayAdapter<String> adapter;
+    final Calendar c = Calendar.getInstance();
+    int currentYear = c.get(Calendar.YEAR);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,20 +42,36 @@ public class YearsFragment extends Fragment{
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
 
+                String valueCell = getItem(position).toString();
+
                 View root = super.getView(position, convertView, parent);
-                if (position<=73) {
+                if (valueCell.equals(String.valueOf(currentYear))) {
                     TextView textView = (TextView) root.findViewById(R.id.tvText);
-                    //textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 25);
-                    textView.setTextColor(getResources().getColor(R.color.textGrid,context.getTheme()));
-                } else if (position>73 && position<=75){
-                    TextView textView = (TextView) root.findViewById(R.id.tvText);
-                    textView.setTextColor(getResources().getColor(R.color.textGridTransperent1,context.getTheme()));
-                } else if (position>75 && position<=77){
-                    TextView textView = (TextView) root.findViewById(R.id.tvText);
-                    textView.setTextColor(getResources().getColor(R.color.textGridTransperent2,context.getTheme()));
-                } else if (position>77){
-                    TextView textView = (TextView) root.findViewById(R.id.tvText);
-                    textView.setTextColor(getResources().getColor(R.color.textGridTransperent3,context.getTheme()));
+                    textView.setTextColor(getResources().getColor(R.color.textGridCurrent, context.getTheme()));
+                    makeTextStrike(valueCell, currentYear, textView);
+                } else {
+                    if (position <= 71) {
+                        TextView textView = (TextView) root.findViewById(R.id.tvText);
+                        //textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 25);
+                        textView.setTextColor(getResources().getColor(R.color.textGrid, context.getTheme()));
+                        makeTextStrike(valueCell, currentYear, textView);
+                    } else if (position > 71 && position <= 73) {
+                        TextView textView = (TextView) root.findViewById(R.id.tvText);
+                        textView.setTextColor(getResources().getColor(R.color.textGridTransperent1, context.getTheme()));
+                        makeTextStrike(valueCell, currentYear, textView);
+                    } else if (position > 73 && position <= 75) {
+                        TextView textView = (TextView) root.findViewById(R.id.tvText);
+                        textView.setTextColor(getResources().getColor(R.color.textGridTransperent2, context.getTheme()));
+                        makeTextStrike(valueCell, currentYear, textView);
+                    } else if (position > 75 && position <= 77) {
+                        TextView textView = (TextView) root.findViewById(R.id.tvText);
+                        textView.setTextColor(getResources().getColor(R.color.textGridTransperent3, context.getTheme()));
+                        makeTextStrike(valueCell, currentYear, textView);
+                    } else if (position > 77) {
+                        TextView textView = (TextView) root.findViewById(R.id.tvText);
+                        textView.setTextColor(getResources().getColor(R.color.textGridTransperent4, context.getTheme()));
+                        makeTextStrike(valueCell, currentYear, textView);
+                    }
                 }
                 return root;
             }
@@ -58,6 +79,12 @@ public class YearsFragment extends Fragment{
         gvYears.setAdapter(adapter);
         adjustGridView();
 
+    }
+
+    public void makeTextStrike(String value, int mYear, TextView textView){
+        if (Integer.parseInt(value)<mYear){
+            textView.setPaintFlags(textView.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        }
     }
 
     private void adjustGridView() {
