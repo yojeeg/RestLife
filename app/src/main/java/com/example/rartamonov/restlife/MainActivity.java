35e74a209654fc5e62fe19614a876062273fec08
Роман <yojeeg@gmail.com>
2017-08-16 17:54:28 +0300

    добавлен класс аутентификации по email

--- app/src/main/java/com/example/rartamonov/restlife/MainActivity.java
+++ app/src/main/java/com/example/rartamonov/restlife/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.rartamonov.restlife;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +18,8 @@ import com.example.rartamonov.restlife.Fragments.MonthsFragment;
 import com.example.rartamonov.restlife.Fragments.TasksFragment;
 import com.example.rartamonov.restlife.Fragments.YearsFragment;
 import com.example.rartamonov.restlife.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity implements DateBornFragment.OnYearsFragmentInteractionListener, YearsFragment.OnMonthsFragmentInteractionListener, MonthsFragment.OnDaysFragmentInteractionListener {
 
